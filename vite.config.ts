5ad6fea00e5d91ea59bd28ef5542f07d3fcf98dc
tyleeer <siradanai.nam@gmail.com>
2023-08-04 13:51:28 +0700

    i gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/thailand-weather-tracker/",
   server: { port: 3000 },
   plugins: [react()],
   resolve: {
